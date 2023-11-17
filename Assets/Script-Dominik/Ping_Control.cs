bd1bd986415de78b5ad88b385c2e78dd5a0f8681
J4ckL4nc3r <151214302+J4ckL4nc3r@users.noreply.github.com>
2023-11-18 00:06:09 +0100

    Tracker für objecttracking

---
+++ Assets/Script-Dominik/Ping_Control.cs
@@ -0,0 +1,26 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ping_Control : MonoBehaviour
+{
+    public float TrackerSpped;
+    public float TrackerDistance;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            TrackerRb.transform (Vector3.);
+        }
+
+    }
+}
